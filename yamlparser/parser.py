539b326ea3ae29a75280a9cd31d3c77479dc818b
Manuel Guenther <siebenkopf@googlemail.com>
2023-11-09 13:55:29 +0100

    Added more documentation; improved packaging

--- yamlparser/parser.py
+++ yamlparser/parser.py
@@ -3,13 +3,40 @@ import sys
 
 from .namespace import NameSpace
 
-def config_parser(parser=None, default_config_file=None, infer_types=True, command_line_options=None, add_config_files=False):
+def config_parser(parser=None, default_config_files=None, infer_types=True, add_config_files=False,  command_line_options=None):
+    """Creates or updates an `argparse.ArgumentParser` with the option to load configuration files (YAML).
+    These files will be automatically parsed and each configuration will be added as a separate option to the command line.
+
+    Arguments:
+
+    parser : argparse.ArgumentParser or None
+    If given, options will be added to that parser. Otherwise, a new parser will be created.
+
+    default_config_files: list or None
+    If given, a list of default configuration files is provided.
+    This feature is untested.
+
+    infer_types: bool
+    If selected (the default), types are inferred from the data types of the configuration file.
+    If disabled, all options will be provided as `str`.
+
+    add_config_files: bool
+    If selected, the list of configuration files is added to the final configuration under the `configuration_files` list.
+
+    command_line_options: list or None
+    For debugging purposes, a list of command line options is accepted. This is mainly a debug and test feature.
+    If not present, `sys.agrv[1:]` is selected, as usual.
+
+    Returns:
+    namespace: NameSpace
+    A namespace object containing all options taken from configuration file and command line.
+    """
     # create the initial parser
     command_line_options = command_line_options or sys.argv[1:]
     # check if the help on the default parser is requested
     requests_help = len(command_line_options) == 0 or len(command_line_options) == 1 and command_line_options[0] in ("-h", "--help")
     _config_parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, add_help=requests_help)
-    _config_parser.add_argument("configuration_files", nargs="+", default=[default_config_file], help="The configuration files to parse. From the second config onward, it be key=value pairs to create sub-configurations")
+    _config_parser.add_argument("configuration_files", nargs="+", default=default_config_files, help="The configuration files to parse. From the second config onward, it be key=value pairs to create sub-configurations")
 
     args,_ = _config_parser.parse_known_args(command_line_options)
     namepsace = NameSpace(args.configuration_files[0])
