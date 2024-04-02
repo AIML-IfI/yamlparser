30495b15dd4b16b93e8a964a64f3e1c0a3c4d3d0
Laurin van den Bergh <laurin.vandenbergh@gmail.com>
2024-04-02 14:06:59 +0200

    convert tests to unittests

--- yamlparser/test/test_argparse.py
+++ yamlparser/test/test_argparse.py
@@ -1,65 +1,62 @@
 import yamlparser
 import os
+import unittest
 
-def test_parser():
-    yaml_file = os.path.join(os.path.dirname(__file__), "test_config.yaml")
-    command_line_options = (yaml_file,)
-    namespace = yamlparser.config_parser(command_line_options=command_line_options, store_config=False)
+class TestArgparse(unittest.TestCase):
 
-    print(namespace.dump())
+    def test_parser(self):
+        yaml_file = os.path.join(os.path.dirname(__file__), "test_config.yaml")
+        command_line_options = (yaml_file,)
+        namespace = yamlparser.config_parser(command_line_options=command_line_options, store_config=False)
 
-def test_parser_help():
-    command_line_options = ("--help",)
-    try:
+        print(namespace.dump())
+
+    def test_parser_help(self):
+        command_line_options = ("--help",)
+        try:
+            namespace = yamlparser.config_parser(command_line_options=command_line_options, store_config=False)
+            print(namespace.dump())
+        except SystemExit:
+            pass
+
+    def test_parser_update_help(self):
+        yaml_file = os.path.join(os.path.dirname(__file__), "test_config.yaml")
+        command_line_options = (yaml_file, "--help")
+        try:
+            namespace = yamlparser.config_parser(command_line_options=command_line_options, store_config=False)
+            print(namespace)
+        except SystemExit:
+            pass
+
+    def test_parser_update(self):
+        yaml_file = os.path.join(os.path.dirname(__file__), "test_config.yaml")
+        command_line_options = (yaml_file, "--name", "UPDATED")
         namespace = yamlparser.config_parser(command_line_options=command_line_options, store_config=False)
         print(namespace.dump())
-    except SystemExit:
-        pass
 
-def test_parser_update_help():
-    yaml_file = os.path.join(os.path.dirname(__file__), "test_config.yaml")
-    command_line_options = (yaml_file, "--help")
-    try:
+    def test_multiple_config(self):
+        yaml_file1 = os.path.join(os.path.dirname(__file__), "test_config.yaml")
+        yaml_file2 = os.path.join(os.path.dirname(__file__), "sub_config.yaml")
+
+        # put both into the same namespace
+        command_line_options = (yaml_file1, yaml_file2)
         namespace = yamlparser.config_parser(command_line_options=command_line_options, store_config=False)
-        print(namespace)
-    except SystemExit:
-        pass
+        self.assertEqual(namespace.name, "test")
+        self.assertEqual(namespace.item, "sub")
 
-def test_parser_update():
-    yaml_file = os.path.join(os.path.dirname(__file__), "test_config.yaml")
-    command_line_options = (yaml_file, "--name", "UPDATED")
-    namespace = yamlparser.config_parser(command_line_options=command_line_options, store_config=False)
-    print(namespace.dump())
+        # put the second into a sub-namespace
+        command_line_options = (yaml_file1, f"minor={yaml_file2}")
+        namespace = yamlparser.config_parser(command_line_options=command_line_options, store_config=False)
+        self.assertEqual(namespace.name, "test")
+        self.assertEqual(namespace.minor.item, "sub")
 
-def test_multiple_config():
-    yaml_file1 = os.path.join(os.path.dirname(__file__), "test_config.yaml")
-    yaml_file2 = os.path.join(os.path.dirname(__file__), "sub_config.yaml")
-
-    # put both into the same namespace
-    command_line_options = (yaml_file1, yaml_file2)
-    namespace = yamlparser.config_parser(command_line_options=command_line_options, store_config=False)
-    assert namespace.name == "test"
-    assert namespace.item == "sub"
-    print(namespace.dump())
-
-    # put the second into a sub-namespace
-    command_line_options = (yaml_file1, f"minor={yaml_file2}")
-    namespace = yamlparser.config_parser(command_line_options=command_line_options, store_config=False)
-    assert namespace.name == "test"
-    assert namespace.minor.item == "sub"
-    print(namespace.dump())
-
-def test_global_config():
-    yaml_file = os.path.join(os.path.dirname(__file__), "test_config.yaml")
-    command_line_options = (yaml_file, "--name", "UPDATED")
-    namespace = yamlparser.config_parser(command_line_options=command_line_options)
-    config = yamlparser.get_config()
-    assert config is namespace
+    def test_global_config(self):
+        yaml_file = os.path.join(os.path.dirname(__file__), "test_config.yaml")
+        command_line_options = (yaml_file, "--name", "UPDATED")
+        namespace = yamlparser.config_parser(command_line_options=command_line_options)
+        config = yamlparser.get_config()
+        self.assertIs(config, namespace)
 
 
 if __name__ == "__main__":
-    test_parser()
-    test_parser_help()
-    test_parser_update_help()
-    test_multiple_config()
-    test_global_config()
+    unittest.main()
--- yamlparser/test/test_yaml.py
+++ yamlparser/test/test_yaml.py
@@ -1,195 +1,168 @@
 import yamlparser
 import os
 import tempfile
+import unittest
 
-def test_yaml_file():
-    # test import from explicit file
-    yaml_file = os.path.join(os.path.dirname(__file__), "test_config.yaml")
-    namespace = yamlparser.NameSpace(yaml_file)
-    assert hasattr(namespace, "name")
+class TestYaml(unittest.TestCase):
 
-    # test import from package with full path
-    namespace = yamlparser.NameSpace("yamlparser @ test/test_config.yaml")
-    assert hasattr(namespace, "name")
+    def test_yaml_file(self):
+        # test import from explicit file
+        yaml_file = os.path.join(os.path.dirname(__file__), "test_config.yaml")
+        namespace = yamlparser.NameSpace(yaml_file)
+        assert hasattr(namespace, "name")
 
-    # test import from package with short path
-    namespace = yamlparser.NameSpace("yamlparser @ test_config.yaml")
-    assert hasattr(namespace, "name")
+        # test import from package with full path
+        namespace = yamlparser.NameSpace("yamlparser @ test/test_config.yaml")
+        self.assertTrue(hasattr(namespace, "name"))
 
-
-def test_yaml_dict():
-    namespace = yamlparser.NameSpace(dict(name="Name", nested=dict(email="name@host.domain")))
-
-    # check that all the values are stored correctly and can be indexed and attributed
-    assert hasattr(namespace, "name")
-    assert namespace.name == "Name"
-    assert namespace["name"] == "Name"
-    assert isinstance(namespace.nested, yamlparser.NameSpace)
-    assert isinstance(namespace["nested"], yamlparser.NameSpace)
-    assert namespace.nested.email == "name@host.domain"
-    assert namespace["nested"]["email"] == "name@host.domain"
+        # test import from package with short path
+        namespace = yamlparser.NameSpace("yamlparser @ test_config.yaml")
+        self.assertTrue(hasattr(namespace, "name"))
 
 
-def test_extend():
-    yaml_file = os.path.join(os.path.dirname(__file__), "test_config.yaml")
-    namespace = yamlparser.NameSpace(yaml_file)
+    def test_yaml_dict(self):
+        namespace = yamlparser.NameSpace(dict(name="Name", nested=dict(email="name@host.domain")))
 
-    # tests adding a new value
-    assert "new_value" not in namespace.dict()
-    namespace.new_value = "New Value"
-    assert hasattr(namespace, "new_value")
-    assert namespace["new_value"] == "New Value"
-    # test the same with indexing
-    assert "other" not in namespace.dict()
-    namespace["other"] = "Other Value"
-    assert hasattr(namespace, "other")
-    assert namespace.other == "Other Value"
-
-    # tests adding a new sub-namespace with a value
-    assert "sub_namespace" not in namespace.dict()
-    namespace.new_subnamespace.name = "New Name"
-    assert hasattr(namespace, "new_subnamespace")
-    assert isinstance(namespace["new_subnamespace"], yamlparser.NameSpace)
-    assert hasattr(namespace["new_subnamespace"], "name")
-    assert namespace["new_subnamespace"]["name"] == "New Name"
-    # test indexing
-    assert "another" not in namespace.dict()
-    namespace["another"] = dict(novel=17)
-    assert hasattr(namespace, "another")
-    assert isinstance(namespace.another, yamlparser.NameSpace)
-    assert hasattr(namespace.another, "novel")
-    assert namespace.another.novel == 17
+        # check that all the values are stored correctly and can be indexed and attributed
+        self.assertTrue(hasattr(namespace, "name"))
+        self.assertEqual(namespace.name, "Name")
+        self.assertEqual(namespace["name"], "Name")
+        self.assertIsInstance(namespace.nested, yamlparser.NameSpace)
+        self.assertIsInstance(namespace["nested"], yamlparser.NameSpace)
+        self.assertEqual(namespace.nested.email, "name@host.domain")
+        self.assertEqual(namespace["nested"]["email"], "name@host.domain")
 
 
-def test_io():
-    namespace = yamlparser.NameSpace(dict(name="Name", nested=dict(email="name@host.domain")))
 
-    try:
-        _,filename = tempfile.mkstemp(".yaml")
-        # save to file
-        namespace.save(filename)
-        # create new namespace from file
-        namespace = yamlparser.NameSpace(filename)
+    def test_extend(self):
+        yaml_file = os.path.join(os.path.dirname(__file__), "test_config.yaml")
+        namespace = yamlparser.NameSpace(yaml_file)
 
-    finally:
-        os.remove(filename)
+        # tests adding a new value
+        self.assertNotIn("new_value", namespace.dict())
+        namespace.new_value = "New Value"
+        self.assertTrue(hasattr(namespace, "new_value"))
+        self.assertEqual(namespace["new_value"], "New Value")
+        # test the same with indexing
+        self.assertNotIn("other", namespace.dict())
+        namespace["other"] = "Other Value"
+        self.assertTrue(hasattr(namespace, "other"))
+        self.assertEqual(namespace.other, "Other Value")
 
-    # check that all elements are loaded back correctly
-    assert hasattr(namespace, "name")
-    assert namespace.name == "Name"
-    assert isinstance(namespace.nested, yamlparser.NameSpace)
-    assert namespace.nested.email == "name@host.domain"
-
-
-def test_format():
-    namespace = yamlparser.NameSpace(dict(name="Name", nested=dict(email="name@host.domain"), value=1.))
-
-    formatted = namespace.format("{name}, {nested.email}, {value}")
-    assert formatted == "Name, name@host.domain, 1.0", formatted
-
-    formatted = namespace.format(["{name}", "{nested.email}", "{value}"])
-    assert formatted == ["Name", "name@host.domain", "1.0"], formatted
-
-    # add some internal names that shall be formatted
-    namespace["my_name"] = "{name}"
-    namespace.nested.my_email = "{nested.email}"
-    namespace.nested["new_email"] = ["{email}"]
-
-    # assert that these elements do not get formatted automatically
-    assert namespace["my_name"] == "{name}"
-    assert namespace.nested.my_email == "{nested.email}"
-    assert namespace.nested["new_email"] == ["{email}"]
-
-    # now, format all samples
-    namespace.format_self()
-
-    # make sure that all elements are formatted correctly
-    assert namespace["my_name"] == "Name"
-    assert namespace.nested.my_email == "name@host.domain"
-    assert namespace.nested["new_email"] == ["name@host.domain"]
+        # tests adding a new sub-namespace with a value
+        self.assertNotIn("new_subnamespace", namespace.dict())
+        namespace.new_subnamespace.name = "New Name"
+        self.assertTrue(hasattr(namespace, "new_subnamespace"))
+        self.assertIsInstance(namespace["new_subnamespace"], yamlparser.NameSpace)
+        self.assertTrue(hasattr(namespace["new_subnamespace"], "name"))
+        self.assertEqual(namespace["new_subnamespace"]["name"], "New Name")
+        # test indexing
+        self.assertNotIn("another", namespace.dict())
+        namespace["another"] = dict(novel=17)
+        self.assertTrue(hasattr(namespace, "another"))
+        self.assertIsInstance(namespace.another, yamlparser.NameSpace)
+        self.assertTrue(hasattr(namespace.another, "novel"))
+        self.assertEqual(namespace.another.novel, 17)
 
 
-def test_freeze():
-    namespace = yamlparser.NameSpace(dict(name="Name", nested=dict(email="name@host.domain"), value=1.))
-    # freeze the namespace
-    namespace.freeze()
+    def test_io(self):
+        namespace = yamlparser.NameSpace(dict(name="Name", nested=dict(email="name@host.domain")))
 
-    # make sure that you can still access the existing attributes
-    _ = namespace.name
-    _ = namespace["nested"]["email"]
+        try:
+            _,filename = tempfile.mkstemp(".yaml")
+            # save to file
+            namespace.save(filename)
+            # create new namespace from file
+            namespace = yamlparser.NameSpace(filename)
+
+        finally:
+            os.remove(filename)
+
+        # check that all elements are loaded back correctly
+        self.assertTrue(hasattr(namespace, "name"))
+        self.assertEqual(namespace.name, "Name")
+        self.assertIsInstance(namespace.nested, yamlparser.NameSpace)
+        self.assertEqual(namespace.nested.email, "name@host.domain")
 
 
-    # assure that modifying or adding features raises an ValueError exceptions
-    try:
-        namespace["new_name"] = "New Name"
-        raise Exception("This should not happen")
-    except AttributeError:
-        pass
+    def test_format(self):
+        namespace = yamlparser.NameSpace(dict(name="Name", nested=dict(email="name@host.domain"), value=1.))
 
-    try:
-        namespace.set("new_name", "New Name")
-        raise Exception("This should not happen")
-    except AttributeError:
-        pass
+        formatted = namespace.format("{name}, {nested.email}, {value}")
+        self.assertEqual(formatted, "Name, name@host.domain, 1.0")
 
-    try:
+        formatted = namespace.format(["{name}", "{nested.email}", "{value}"])
+        self.assertEqual(formatted, ["Name", "name@host.domain", "1.0"])
+
+        # add some internal names that shall be formatted
+        namespace["my_name"] = "{name}"
+        namespace.nested.my_email = "{nested.email}"
+        namespace.nested["new_email"] = ["{email}"]
+
+        # assert that these elements do not get formatted automatically
+        self.assertEqual(namespace["my_name"], "{name}")
+        self.assertEqual(namespace.nested.my_email, "{nested.email}")
+        self.assertEqual(namespace.nested["new_email"], ["{email}"])
+
+        # now, format all samples
+        namespace.format_self()
+
+        # make sure that all elements are formatted correctly
+        self.assertEqual(namespace["my_name"], "Name")
+        self.assertEqual(namespace.nested.my_email, "name@host.domain")
+        self.assertEqual(namespace.nested["new_email"], ["name@host.domain"])
+
+
+    def test_freeze(self):
+        namespace = yamlparser.NameSpace(dict(name="Name", nested=dict(email="name@host.domain"), value=1.))
+        # freeze the namespace
+        namespace.freeze()
+
+        # make sure that you can still access the existing attributes
+        _ = namespace.name
+        _ = namespace["nested"]["email"]
+
+
+        # assure that modifying or adding features raises an ValueError exceptions
+        with self.assertRaises(AttributeError):
+            namespace.new_name = "New Name"
+            namespace.set("new_name", "New Name")
+            namespace.name = "New Name"
+            namespace["name"] = "New Name"
+            
+        # try to unfreeze
+        namespace.unfreeze()
         namespace.new_name = "New Name"
-        raise Exception("This should not happen")
-    except AttributeError:
-        pass
-
-    try:
-        namespace.name = "New Name"
-        raise Exception("This should not happen")
-    except AttributeError:
-        pass
-
-    try:
-        namespace["name"] = "New Name"
-        raise Exception("This should not happen")
-    except AttributeError:
-        pass
-
-    # try to unfreeze
-    namespace.unfreeze()
-    namespace.new_name = "New Name"
 
 
-def test_sub_namespace():
-    # create a namespace that loads another yaml file
-    namespace = yamlparser.NameSpace(dict(
-        name="Name",
-        nested={"yaml":os.path.join(os.path.dirname(__file__), "test_config.yaml"), "name": "new_name"},
-        value=1.
-    ))
-    namespace.freeze()
-    assert hasattr(namespace, "nested")
-    assert hasattr(namespace.nested, "name")
-    assert namespace.nested.name == "new_name"
-    assert hasattr(namespace.nested, "pi")
-    assert hasattr(namespace.nested, "nested")
-    assert hasattr(namespace.nested.nested, "name")
+    def test_sub_namespace(self):
+        # create a namespace that loads another yaml file
+        namespace = yamlparser.NameSpace(dict(
+            name="Name",
+            nested={"yaml":os.path.join(os.path.dirname(__file__), "test_config.yaml"), "name": "new_name"},
+            value=1.
+        ))
+        namespace.freeze()
+        self.assertEqual(namespace.nested.name, "new_name")
+        self.assertTrue(hasattr(namespace, "nested"))
+        self.assertTrue(hasattr(namespace.nested, "name"))
+        self.assertTrue(hasattr(namespace.nested, "pi"))
+        self.assertTrue(hasattr(namespace.nested, "nested"))
+        self.assertTrue(hasattr(namespace.nested.nested, "name"))
 
-
-    namespace = yamlparser.NameSpace(dict(
-        name="Name",
-        nested={"yaml":"yamlparser@test_config.yaml"},
-        value=1.
-    ))
-    namespace.freeze()
-    assert hasattr(namespace, "nested")
-    assert hasattr(namespace.nested, "name")
-    assert hasattr(namespace.nested, "pi")
-    assert hasattr(namespace.nested, "nested")
-    assert hasattr(namespace.nested.nested, "name")
+        namespace = yamlparser.NameSpace(dict(
+            name="Name",
+            nested={"yaml":"yamlparser@test_config.yaml"},
+            value=1.
+        ))
+        namespace.freeze()
+        self.assertTrue(hasattr(namespace, "nested"))
+        self.assertTrue(hasattr(namespace.nested, "name"))
+        self.assertTrue(hasattr(namespace.nested, "pi"))
+        self.assertTrue(hasattr(namespace.nested, "nested"))
+        self.assertTrue(hasattr(namespace.nested.nested, "name"))
 
 
 
 if __name__ == "__main__":
-    test_yaml_file()
-    test_yaml_dict()
-    test_extend()
-    test_io()
-    test_format()
-    test_freeze()
-    test_sub_namespace()
+    unittest.main()
