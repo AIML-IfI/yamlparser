6d93b9ceafe9b8f41c665730f7c923e742a3789a
Laurin van den Bergh <laurin.vandenbergh@gmail.com>
2024-04-02 14:25:07 +0200

    make all argparse tests rely on some boolean eval rather than visually evaluating print statements

--- yamlparser/test/test_argparse.py
+++ yamlparser/test/test_argparse.py
@@ -9,30 +9,37 @@ class TestArgparse(unittest.TestCase):
         command_line_options = (yaml_file,)
         namespace = yamlparser.config_parser(command_line_options=command_line_options, store_config=False)
 
-        print(namespace.dump())
+        expected = yamlparser.NameSpace({
+            "name": "test",
+            "int_value": 42,
+            "list_value": [10, 42, 101],
+            "nested": {
+                "name": "nested_test",
+                "pi": 3.14159265,
+                "nested": {
+                    "name": "subnested"
+                },
+            }
+        })
+        self.assertEqual(namespace.dump(), expected.dump())
 
     def test_parser_help(self):
         command_line_options = ("--help",)
-        try:
+        with self.assertRaises(SystemExit):
             namespace = yamlparser.config_parser(command_line_options=command_line_options, store_config=False)
-            print(namespace.dump())
-        except SystemExit:
-            pass
 
     def test_parser_update_help(self):
         yaml_file = os.path.join(os.path.dirname(__file__), "test_config.yaml")
         command_line_options = (yaml_file, "--help")
-        try:
+        with self.assertRaises(SystemExit):
             namespace = yamlparser.config_parser(command_line_options=command_line_options, store_config=False)
-            print(namespace)
-        except SystemExit:
-            pass
 
     def test_parser_update(self):
         yaml_file = os.path.join(os.path.dirname(__file__), "test_config.yaml")
         command_line_options = (yaml_file, "--name", "UPDATED")
         namespace = yamlparser.config_parser(command_line_options=command_line_options, store_config=False)
-        print(namespace.dump())
+        self.assertTrue(hasattr(namespace, "name"))
+        self.assertTrue(hasattr(namespace, "UPDATED"))
 
     def test_multiple_config(self):
         yaml_file1 = os.path.join(os.path.dirname(__file__), "test_config.yaml")
